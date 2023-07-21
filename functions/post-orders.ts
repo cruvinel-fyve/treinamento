67d4bd1ca57c23d527b8534ea68b3ae8822e03a6
Nicolas Ferreira Cruvinel Monteiro Souza <nicolas.cruvinel@fyve.com.br>
2023-07-21 13:21:46 -0300

    debug-function-post-orders

--- functions/post-orders.ts
+++ functions/post-orders.ts
@@ -8,128 +8,133 @@ const dynamodb = DynamoDBDocumentClient.from(client); // client is DynamoDB clie
 
 export async function postOrders (event:any): Promise<any> {
     console.log("event: ", event);
-    // console.log("evenRb: ", event.Records[0].body);
+     console.log("evenRb: ", event.Records[0].body);
     const eventParse = JSON.parse(event.Records[0].body);
     console.log("evenParse: ", eventParse);
-    const productInOrder = eventParse.queryStringParameters;
-    const products = await getDataProduct(productInOrder.id_user);
-    //////////////////////////////////////////////
-    // Fazer lógica para calcular valor da venda//
-    //////////////////////////////////////////////
-    const cep = productInOrder.cep;
-    const cepTratado = cep.replace(/\D/g, '');
-
-    // verificando se ha erros de badRequest
-    const error = validation(productInOrder, products); 
-    
-    const freight = calcFrete(cepTratado)
-    if(await freight == "invalid cep!"){error.body = await freight;}
-    
-    if (error.body == "notErrorRequest" || error.body != "invalid cep!") { // validação
-        //const priceOrder = priceProductOrder + freight;
-        const params = {
-            TableName: 'orders-cdk',
-            Item: {
-                userId: productInOrder.id_user,
-                date: new Date().toISOString(),
-                products: products,
-                //priceProducts: priceProductOrder,
-                //priceOrder: priceOrder,
-            }
-        };
-        if(products){
-            let j = 0;
-            for (const product of products) {
-                const response = await modifyStock(product);
-                if(response.body == "ConditionalCheckFailedException"){
-                    j++;
-                    // console.log("Passei aqui, valor do J: ", j);
-                }
-            }
-            
-            if(j >= 0){
+    const infoUser = eventParse.queryStringParameters;
+    console.log("productInOrder: ", infoUser);
+    if (infoUser != undefined){
+        const products = await getProductsInCart(infoUser.id_user);
+        console.log ("products: ", products)
+        const errorr = await validation(infoUser, products);
+        if(products != undefined && errorr != undefined) {
+            console.log("error.statusCode: ",errorr.statusCode)
+            if (errorr.statusCode == 200){
+                const freight = await calcFrete(infoUser.cep)
+                const priceProductOrder = priceOfAllProducts(products);
+                const priceOrder = Number(priceProductOrder)+Number(freight);
+                console.log('Entrando aqui:SE');
+                const params = {
+                    TableName: 'orders-cdk',
+                    Item: {
+                        userId: infoUser.id_user,
+                        date: new Date().toISOString(),
+                        products: products,
+                        priceProducts: priceProductOrder,
+                        freight: freight,
+                        priceOrder: priceOrder,
+                        // adicionar endereço
+                    }
+                };
+                    
                 const input = new PutCommand(params);
                 // console.log(params.Item);
                 await dynamodb.send(input);
                 // mudando status do carrinho para disable
                 // console.log("products: ", products);
-                for (const product of products) await putDisabledStatus(product);    
-        
+                for (const product of products) {await putDisabledStatus(product)};
+            
                 const response = {
                     statusCode: 200,
                     body: "item added successfully!"
                 };
-                
+                        
                 return response;
             } else {
-                
-                return "ERRO:ConditionalCheckFailedException"
+                console.log('Entrando aqui:SENAO')
+                console.log("error: ", errorr);
+                return errorr;
+                    
             }   
-        }
-    }
-    else {
-        
-        return error; // erro dado pela validação 
+        } else {
+            return 'product or validation is undefined'
+        } 
+    } else {
+        return 'input is undefined'
     }
 }
 // Validando parametros
-const validation = (params:any, products:any) => {
+const validation = async (params:any, products:any) => {
+    let j = 0;
+    for (const product of products) {
+        const response = await modifyStock(product);
+            if(response.body == "ConditionalCheckFailedException"){
+            j++;
+            // console.log("Passei aqui, valor do J: ", j);
+        }
+    }
+    
+    
     if (!params.id_user) {
         const response = {
             statusCode: 400,
             body: JSON.stringify({ message: "ERRO:Missing id_user" }),
         };
         return response;
-    }
-    // else if (!params.id_product) {
-    //     const response = {
-    //         statusCode: 400,
-    //         body: JSON.stringify({ message: "Missing id_product" }),
-    //     };
-    //     return response;
-    // } 
-    else if (products.length == 0) {
+        
+    } else if (j>0) {
+         const response = {
+             statusCode: 400,
+             body: JSON.stringify({ message: "ERRO: Stock not enough!" }),
+         };
+         return response;
+         
+    } else if (products.length == 0) {
         const response = {
             statusCode: 400,
             body: JSON.stringify({ message: "ERRO:No product in cart!" }),
         };
         return response;
-    }else {
+        
+    } else if (await calcFrete(params.cep) == "invalid cep!") {
+        const response = {
+            statusCode: 400,
+            body: JSON.stringify({ message: "ERRO:invalid cep!" }),
+        };
+        return response;
+        
+    } else {
         const response = {
             statusCode: 200,
-            body: JSON.stringify({ message: "MESSAGE:notErrorRequest!" }),
+            body: JSON.stringify({ message: "Message: No error request" }),
         };
         return response;
     }
 };
 
-const getDataProduct = async (id_user:string) => {
+const getProductsInCart = async (id_user:any) => {
     
     const params = new QueryCommand({
         TableName: 'cart-cdk',
-        KeyConditionExpression: '#id_user = :id_user',
+        KeyConditionExpression: 'userId = :id_user',
         ExpressionAttributeValues: {
             ':id_user': id_user,
-        },
-        ExpressionAttributeNames: {
-            '#id_user': 'userId'
         }
     });
-
+    
     const responsedb = await dynamodb.send(params);
     const activedItem = responsedb.Items?.map(element =>(productActivedInCart(element)));
-    // console.log("activedItem: ", activedItem);
+    console.log("activedItem: ", activedItem);
+    if (activedItem != undefined) {
+        const dados = responsedb.Items?.filter((item, index) => {
     
-    const dados = responsedb.Items?.filter((item, index) => {
-        if(activedItem){
             return activedItem[index];
-        } else {
-            return "Nenhum produto encontrado!"
-        }
-        
-    });
-    
-    return dados;
+        });
+
+        return dados;
+    } else {
+        return 'ERRO:No product in cart!'
+    }
 };
 
 const productActivedInCart = (db:any) =>{
@@ -146,42 +151,45 @@ const productActivedInCart = (db:any) =>{
 };
 
 const putDisabledStatus = async (element:any) => {
-    // console.log("element.id_product(put): ", element.id_product);
-    const params = {
-        TableName: 'cart-cdk',
-        Item: {
-            image: element.image,
-            category: element.category,
-            name: element.name,
-            userId: element.id_user,
-            productId: element.productId,
-            qtd_product: 0,
-            size: element.size,
-            amount: element.amount,
-            date_expiration: element.date_expiration,
-            actived: false
-        }
-    };
-    // console.log(params.Item);
-    
-    const input = new PutCommand(params);
+    if (element != undefined) {
+        console.log("element(put): ", element);
+        const params = {
+            TableName: 'cart-cdk',
+            Item: {
+                image: element.image,
+                category: element.category,
+                name: element.name,
+                userId: element.userId,
+                productId: element.productId,
+                qtd_product: 0,
+                size: element.size,
+                amount: element.amount,
+                date_expiration: element.date_expiration,
+                actived: false
+            }
+        };
+        // console.log(params.Item);
         
-    await dynamodb.send(input);
-    // console.log("responsedbput: ", responsedb);
+        const input = new PutCommand(params);
+            
+        await dynamodb.send(input);
+ 
+    } else {
+        console.log ('Product is undefined')
+    }
 };
 
 const modifyStock = async (element:any) => {
-    //console.log("element.id_product(modify): ", element.id_product);
+    console.log("element(modify): ", element);
     try{
         var params = {
             TableName: 'products-cdk',
-            Key: { id: element.id_product },
+            Key: { id : element.productId },
             UpdateExpression: 'set #qtdStock = #qtdStock - :qtdSale',
-            ConditionExpression: '#qtdStock > :minInStock',
+            ConditionExpression: '#qtdStock > :qtdSale ',
             ExpressionAttributeNames: {'#qtdStock' : 'qtd_estoque'},
             ExpressionAttributeValues: {
                 ':qtdSale' : element.qtd_product,
-                ':minInStock' : 0,
             },
             ReturnValuesOnConditionCheckFailure: 'ALL_OLD'
         };
@@ -210,7 +218,7 @@ const modifyStock = async (element:any) => {
     }
 };
 
-const calcFrete = async (cepTratado:string): Promise<string> => {
+const calcFrete = async (cepTratado:any) => {
     // console.log("cepTratado(calcFrete):", cepTratado)
     var validacep = /^[0-9]{8}$/;
     
@@ -224,10 +232,18 @@ const calcFrete = async (cepTratado:string): Promise<string> => {
         const ddd = callback.data.ddd;
         const frete = Number(ddd)/10*3;
         
-        return frete.toFixed(2);
+        return String(frete.toFixed(2));
     } else {
         // console.log("SENAO");
         return "invalid cep!";
     }
     
 };
+
+const priceOfAllProducts = (products:any) => {
+    let price = 0;
+    for (const product of products){
+        price += product.amount*product.qtd_product;
+    } 
+    return price;
+}
